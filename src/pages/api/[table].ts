4eed21baeff1796e893ba792d7f9f19c4bb42f52
Fajar Nur Prasetyo <fajarnurprasetyo.2@gmail.com>
2024-08-02 11:09:01 +0700

    v1.0.0

---
+++ src/pages/api/[table].ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const table = req.query.table as string;
+
+  switch (req.method) {
+    case "PUT":
+      try {
+        // @ts-expect-error
+        const data = await prisma[table].create({data: req.body});
+        res.status(200).json({success: true, data});
+      } catch (error) {
+        res.status(400).json({success: false, error});
+      }
+      break;
+    case "PATCH":
+      try {
+        if (!req.query.id) throw "Parameter 'id' is required!";
+        // @ts-expect-error
+        const data = await prisma[table].update({
+          where: {id: req.query.id},
+          data: req.body,
+        });
+        res.status(200).json({success: true, data});
+      } catch (error) {
+        res.status(400).json({success: false, error});
+      }
+      break;
+    case "GET":
+      try {
+        let data = null;
+        if (req.query.id) {
+          // @ts-expect-error
+          data = await prisma[table].findUnique({
+            where: {id: req.query.id},
+            ...req.body,
+          });
+        } else {
+          // @ts-expect-error
+          data = await prisma[table].findMany(req.body);
+        }
+        if (data !== null) {
+          res.status(200).json({success: true, data});
+        } else {
+          res.status(404).json({success: false, error: "Not found!"});
+        }
+      } catch (error) {
+        res.status(400).json({success: false, error});
+      }
+      break;
+    case "DELETE":
+      try {
+        if (!req.query.id) throw "Parameter 'id' is required!";
+        // @ts-expect-error
+        const data = await prisma[table].delete({
+          where: {id: req.query.id},
+        });
+        res.status(200).json({success: true, data});
+      } catch (error) {
+        res.status(404).json({success: false, error: "Not found!"});
+      }
+      break;
+    default:
+      res.status(405).json({success: false, error: "Method not allowed!"});
+      break;
+  }
+}
